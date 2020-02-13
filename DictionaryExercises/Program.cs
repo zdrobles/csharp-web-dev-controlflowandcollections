41155c1284cd9ec661c20c0d134a93227461fdfc
Carly Langlois <cj.langlois.cl@gmail.com>
2020-02-13 15:47:03 -0600

    once again add dictionaries

---
+++ DictionaryExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DictionaryExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
